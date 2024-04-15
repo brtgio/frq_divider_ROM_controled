f53367d99c6ae71acb4ce8a0ed49a41a4c599627
brtgio <67073086+brtgio@users.noreply.github.com>
2024-04-15 12:48:51 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,7 +2,7 @@
 
 `define default_netname none
 
-module tt_um_frq_divider (
+module tt_um_frq_divider(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
