413e87c8e7495d6d4882e293ccb8d30f37c8b99b
brtgio <67073086+brtgio@users.noreply.github.com>
2024-04-15 12:56:02 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,7 +15,7 @@ module tt_um_frq_divider(
     top frq_divider(
 
         .clk(uio_in[0]),
-        .reset_n(uio_in[5]),
+        .reset_n(ui_in[5]),
         .F_select(ui_in[4:0]),
         .clk_out(uio_in[1])
         
